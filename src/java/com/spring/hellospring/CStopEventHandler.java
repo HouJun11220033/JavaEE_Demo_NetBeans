f6fff10f4fe9cd2d1be1f74a0f4c710e15251d43
HouJun11220033 <1144724015@qq.com>
2016-07-22 22:07:01 +0800

    Event Signed-off-by:HouJun11220033 <1144724015@qq.com>

---
+++ src/java/com/spring/hellospring/CStopEventHandler.java
@@ -0,0 +1,15 @@
+package com.spring.hellospring;
+
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextStoppedEvent;
+
+public class CStopEventHandler implements ApplicationListener<ContextStoppedEvent> {
+
+    @Override
+    public void onApplicationEvent(ContextStoppedEvent event) {
+        System.out.println("ContextStoppedEvent Received");
+
+    }
+
+}
--- src/java/com/spring/hellospring/MainApp.java
+++ src/java/com/spring/hellospring/MainApp.java
@@ -2,31 +2,37 @@ package com.spring.hellospring;
 
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainApp {
 
     public static void main(String[] args) {
+        ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
         ApplicationContext ac = new ClassPathXmlApplicationContext("Beans.xml");
         ApplicationContext ac_1 = new AnnotationConfigApplicationContext(HelloWorldConfig.class);
         ApplicationContext ac_2 = new AnnotationConfigApplicationContext(TextEditorConfig.class);
-        AbstractApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+        //AbstractApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+        CustomEventPublisher cep = (CustomEventPublisher) context.getBean("customEventPublisher");
+        cep.publish();
+        cep.publish();
+        //context.start();
         //换用AbstractApplicationContext对象时，可以调用销毁方法。。。
         //在非Web应用中，手工加载Spring IoC容器，不能用ApplicationContext，要用AbstractApplicationContext。用完以后要记得调用ctx.close()关闭容器。如果不记得关闭容器，最典型的问题就是数据库连接不能释放
         //  HelloWorld hw = (HelloWorld) context.getBean("helloworld_1");
         //  HelloWorld hw_1 = (HelloWorld) context.getBean("helloworld");
         //  HelloChina hc_1 = (HelloChina) context.getBean("hellochina");
-        HelloWorld helloworld = ac_1.getBean(HelloWorld.class);
-        TextEditor te = ac_2.getBean(TextEditor.class);
+        //  HelloWorld helloworld = ac_1.getBean(HelloWorld.class);
+        //  TextEditor te = ac_2.getBean(TextEditor.class);
 
         //  TextEditor te = (TextEditor) ac.getBean("textEditor");
-        helloworld.setMessage1("Hello World!");
-        helloworld.getMessage1();
+        //  helloworld.setMessage1("Hello World!");
+        //  helloworld.getMessage1();
+        //  context.stop();
         // Profile pro=(Profile) ac.getBean("profile");
         //  JavaCollection jc = (JavaCollection) ac.getBean("javaCollection");
 //        Student stu = (Student) ac.getBean("student");
-
         // hw.setMessage("I am object hw");
         //hw.destory();
         //  hw.getMessage();
@@ -46,7 +52,7 @@ public class MainApp {
 //        jc.getAddrSet();
 //        System.out.println("Name : " + stu.getName());
 //        System.out.println("Age : " + stu.getAge());
-        te.spellCheck();
+        //  te.spellCheck();
         // pro.printAge();
         //pro.printName();
         //  context.registerShutdownHook();
