82ad5dbdd3f5db95ee3063c4e18c233d808b3e13
HouJun11220033 <1144724015@qq.com>
2016-08-01 18:28:02 +0800

    CURD Signed-off-by:HouJun11220033 <1144724015@qq.com>

--- src/java/com/spring/tutorialspoint/controller/HelloController.java
+++ src/java/com/spring/tutorialspoint/controller/HelloController.java
@@ -1,19 +1,19 @@
-//package com.spring.tutorialspoint.controller;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestMethod;
-//import org.springframework.ui.ModelMap;
-//
-//@Controller
-//@RequestMapping("/hello")
-//public class HelloController {
-//
-//    @RequestMapping(method = RequestMethod.GET)
-//    public String printHello(ModelMap model) {
-//        model.addAttribute("message", "Hello Spring MVC Framework!");
-//
-//        return "hello";
-//    }
-//
-//}
+package com.spring.tutorialspoint.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.ModelMap;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String printHello(ModelMap model) {
+        model.addAttribute("message", "Hello Spring MVC Framework!");
+
+        return "hello";
+    }
+
+}
