8b02ca9f2c70aa720747787753d39106dc86db22
HouJun11220033 <1144724015@qq.com>
2016-08-04 23:51:21 +0800

    HttpMessageConverter Signed-off-by:HouJun11220033 <1144724015@qq.com>

--- src/java/com/spring/guigu/controller/Controller_2.java
+++ src/java/com/spring/guigu/controller/Controller_2.java
@@ -2,25 +2,70 @@ package com.spring.guigu.controller;
 
 import com.spring.guigu.dao.EmployeeDao;
 import com.spring.guigu.model.Employee;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collection;
+import java.util.Date;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
 
 @Controller
 public class Controller_2 {
-    
+
     @Autowired
     private EmployeeDao employeeDao;
-//    @Autowired
-//    private ResourceBundleMessageSource messageSource;
-    
+    @Autowired
+    private ResourceBundleMessageSource messageSource;
+
+    @RequestMapping("/testResponseEntity")
+    public ResponseEntity<byte[]> testResponseEntity(HttpSession session) throws IOException {
+        byte[] body = null;
+        ServletContext servletContext = session.getServletContext();
+        InputStream in = servletContext.getResourceAsStream("/files/abc.txt");
+        body = new byte[in.available()];
+        in.read(body);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Disposition", "attachment;filename=abc.txt");
+
+        HttpStatus statusCode = HttpStatus.OK;
+
+        ResponseEntity<byte[]> response = new ResponseEntity<byte[]>(body, headers, statusCode);
+        return response;
+    }
+
+    @ResponseBody
+    @RequestMapping("/testHttpMessageConverter")
+    public String testHttpMessageConverter(@RequestBody String body) {
+        System.out.println(body);
+        return "HelloWorld !" + new Date();
+    }
+
+    @ResponseBody
+    @RequestMapping("/testJson")
+    public Collection<Employee> testJson() {
+        return employeeDao.getAll();
+
+        //return "index";
+    }
+
     @RequestMapping("/testConversionServiceConverer")
     public String testConverter(@RequestParam Employee employee) {
         System.out.println("Save:" + employee);
         employeeDao.save(employee);
         return "redirect:/emps";
     }
-    
+
 }
---
+++ web/index.jsp
@@ -0,0 +1,38 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+     <head>
+          <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+          <title>Insert title here</title>
+          <script type="text/javascript" src="scripts/jquery-1.9.1.min.js"></script>
+          <script type="text/javascript">
+              $(function () {
+                  $("#testJson").click(function () {
+                      var url = this.href;
+                      var args = {};
+                      $.post(url, args, function (data) {
+                          for (var i = 0; i < data.length; i++) {
+                              var id = data[i].id;
+                              var lastName = data[i].lastName;
+
+                              alert(id + ": " + lastName);
+                          }
+                      });
+                      return false;
+                  });
+              })
+          </script>
+     </head>
+     <body>
+          <h1>Hello World!</h1>
+          
+          <form action="testHttpMessageConverter" method="POST" enctype="multipart/form-data">
+               File :<input type="file" name="file"/>
+               Desc :<input type="text" name="desc"/>
+               <input type="submit" value="Submit"/>
+          </form>
+          <!--href 难道还不能加上“/” ???-->
+          <a href="testJson" id="testJson">Test Json</a><br>
+          <a href="testResponseEntity">Test ResponseEntity</a><br>
+     </body>
+</html>
